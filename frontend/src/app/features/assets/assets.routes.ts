fc4e3a99ac4e74766aad4abb65bfe905534a435f
Membrive <membri_2@hotmail.com>
2025-08-11 20:01:41 +0200

    refactor(frontend/assets): tidy routes and enable edit path

--- frontend/src/app/features/assets/assets.routes.ts
+++ frontend/src/app/features/assets/assets.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const ASSET_ROUTES: Routes = [
-  { path: '',    loadComponent: () => import('./asset-list.component').then(m => m.default) },
-  { path: 'new', loadComponent: () => import('./asset-form.component').then(m => m.default) },
+  { path: '',            loadComponent: () => import('./asset-list.component').then(m => m.default) },
+  { path: 'new',         loadComponent: () => import('./asset-form.component').then(m => m.default) },
+  { path: ':id/edit',    loadComponent: () => import('./asset-form.component').then(m => m.default) },
 ];
